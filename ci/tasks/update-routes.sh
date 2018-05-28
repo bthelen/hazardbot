2a3e73d8978be56bf2be8af2c832f5aab4cbedd1
Bruce E. Thelen <bthelen@pivotal.io>
2018-05-28 12:56:00 -0500

    Update reroute logic and pass app name

--- ci/tasks/update-routes.sh
+++ ci/tasks/update-routes.sh
@@ -14,13 +14,13 @@ cf apps
 cf routes
 
 export PWS_DOMAIN_NAME=$PWS_APP_DOMAIN
-export MAIN_ROUTE_HOSTNAME=main-$PWS_APP_SUFFIX
+export MAIN_ROUTE_HOSTNAME=$APP_NAME-$PWS_APP_SUFFIX
 
 export NEXT_APP_COLOR=$(cat ./current-app-info/next-app.txt)
-export NEXT_APP_HOSTNAME=$NEXT_APP_COLOR-$PWS_APP_SUFFIX
+export NEXT_APP_HOSTNAME=$APP_NAME-$PWS_APP_SUFFIX-$NEXT_APP_COLOR
 
 export CURRENT_APP_COLOR=$(cat ./current-app-info/current-app.txt)
-export CURRENT_APP_HOSTNAME=$CURRENT_APP_COLOR-$PWS_APP_SUFFIX
+export CURRENT_APP_HOSTNAME=$APP_NAME-$PWS_APP_SUFFIX-$CURRENT_APP_COLOR
 
 echo "Mapping main app route to point to $NEXT_APP_HOSTNAME instance"
 cf map-route $NEXT_APP_HOSTNAME $PWS_DOMAIN_NAME --hostname $MAIN_ROUTE_HOSTNAME
