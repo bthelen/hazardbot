dfbd386539b40e37210587d04337d7ddcdf3df32
Bruce E. Thelen <bthelen@pivotal.io>
2018-05-28 12:00:44 -0500

    Fix paths

--- ci/tasks/prepare-app-manifest.sh
+++ ci/tasks/prepare-app-manifest.sh
@@ -7,7 +7,11 @@ cat ./current-app-info/current-app.txt
 pushd build-artifacts
 tar zxvf $APP_NAME-*.tgz
 
-sed "s/$APP_NAME-$PWS_APP_SUFFIX/$APPNAME-$PWS_APP_SUFFIX-$(cat ./current-app-info/next-app.txt)/" ./manifests/manifest.yml > ./manifests/manifest-bg.yml
+sed "s/$APP_NAME-$PWS_APP_SUFFIX/$APPNAME-$PWS_APP_SUFFIX-$(cat ../current-app-info/next-app.txt)/" ./manifests/manifest.yml \
+    sed "s/#TWILIO_AUTH_TOKEN#/$TWILIO_AUTH_TOKEN/g" \
+    sed "s/#TWILIO_PHONE_NUMBER#/$TWILIO_PHONE_NUMBER/g" \
+    sed "s/#TWILIO_ACCOUNT_SID#/$TWILIO_ACCOUNT_SID/g" \
+    > ./manifests/manifest-bg.yml
 cp ./manifests/manifest-bg.yml ../app-manifest-output/
 cp ./build/libs/$APP_NAME-*.jar ../app-manifest-output/
 popd
