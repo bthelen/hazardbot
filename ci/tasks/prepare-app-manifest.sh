664c51a0e27186c9e312240943ed6fd027189668
Bruce E. Thelen <bthelen@pivotal.io>
2018-05-28 11:23:20 -0500

    Update paths

--- ci/tasks/prepare-app-manifest.sh
+++ ci/tasks/prepare-app-manifest.sh
@@ -4,6 +4,10 @@ set -xe
 
 cat ./current-app-info/current-app.txt
 
-sed "s/$APPNAME-$PWS_APP_SUFFIX/$APPNAME-$PWS_APP_SUFFIX-$(cat ./current-app-info/next-app.txt)/" ./build-artifacts/manifest.yml > ./build-artifacts/manifest-bg.yml
+pushd build-artifacts
+tar zxvf $APP_NAME-*.tgz
 
-cat ./build-artifacts/manifest-bg.yml
+sed "s/$APP_NAME-$PWS_APP_SUFFIX/$APPNAME-$PWS_APP_SUFFIX-$(cat ./current-app-info/next-app.txt)/" ./manifests/manifest.yml > ./manifests/manifest-bg.yml
+popd
+
+cat ./build-artifacts/manifests/manifest-bg.yml
