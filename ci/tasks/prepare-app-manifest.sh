e819a094cef916047e76b0628699737793302ff6
Bruce E. Thelen <bthelen@pivotal.io>
2018-05-28 12:06:45 -0500

    Forgot pipes

--- ci/tasks/prepare-app-manifest.sh
+++ ci/tasks/prepare-app-manifest.sh
@@ -7,9 +7,9 @@ cat ./current-app-info/current-app.txt
 pushd build-artifacts
 tar zxvf $APP_NAME-*.tgz
 
-sed "s/$APP_NAME-$PWS_APP_SUFFIX/$APPNAME-$PWS_APP_SUFFIX-$(cat ../current-app-info/next-app.txt)/" ./manifests/manifest.yml \
-    sed "s/#TWILIO_AUTH_TOKEN#/$TWILIO_AUTH_TOKEN/g" \
-    sed "s/#TWILIO_PHONE_NUMBER#/$TWILIO_PHONE_NUMBER/g" \
+sed "s/$APP_NAME-$PWS_APP_SUFFIX/$APPNAME-$PWS_APP_SUFFIX-$(cat ../current-app-info/next-app.txt)/" ./manifests/manifest.yml | \
+    sed "s/#TWILIO_AUTH_TOKEN#/$TWILIO_AUTH_TOKEN/g" | \
+    sed "s/#TWILIO_PHONE_NUMBER#/$TWILIO_PHONE_NUMBER/g" | \
     sed "s/#TWILIO_ACCOUNT_SID#/$TWILIO_ACCOUNT_SID/g" \
     > ./manifests/manifest-bg.yml
 cp ./manifests/manifest-bg.yml ../app-manifest-output/
