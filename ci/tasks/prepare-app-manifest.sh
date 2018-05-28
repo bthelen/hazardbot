08438a8a46f4f18a487452925b62640ebe9a30ec
Bruce E. Thelen <bthelen@pivotal.io>
2018-05-28 12:25:41 -0500

    fix app name reference

--- ci/tasks/prepare-app-manifest.sh
+++ ci/tasks/prepare-app-manifest.sh
@@ -7,7 +7,7 @@ cat ./current-app-info/current-app.txt
 pushd build-artifacts
 tar zxvf $APP_NAME-*.tgz
 
-sed "s/$APP_NAME-$PWS_APP_SUFFIX/$APPNAME-$PWS_APP_SUFFIX-$(cat ../current-app-info/next-app.txt)/" ./manifests/manifest.yml | \
+sed "s/$APP_NAME-$PWS_APP_SUFFIX/$APP_NAME-$PWS_APP_SUFFIX-$(cat ../current-app-info/next-app.txt)/" ./manifests/manifest.yml | \
     sed "s/#TWILIO_AUTH_TOKEN#/$TWILIO_AUTH_TOKEN/g" | \
     sed "s/#TWILIO_PHONE_NUMBER#/$TWILIO_PHONE_NUMBER/g" | \
     sed "s/#TWILIO_ACCOUNT_SID#/$TWILIO_ACCOUNT_SID/g" \
