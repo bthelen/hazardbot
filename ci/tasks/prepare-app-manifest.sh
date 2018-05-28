c46e2468f5a1e0d6bd448c02aef6ca74a7bc4211
Bruce E. Thelen <bthelen@pivotal.io>
2018-05-28 11:39:29 -0500

    Put the manifest and jar in the app-manifest-output directory

--- ci/tasks/prepare-app-manifest.sh
+++ ci/tasks/prepare-app-manifest.sh
@@ -8,6 +8,8 @@ pushd build-artifacts
 tar zxvf $APP_NAME-*.tgz
 
 sed "s/$APP_NAME-$PWS_APP_SUFFIX/$APPNAME-$PWS_APP_SUFFIX-$(cat ./current-app-info/next-app.txt)/" ./manifests/manifest.yml > ./manifests/manifest-bg.yml
+cp ./manifests/manifest-bg.yml ../app-manifest-output/
+cp ./build/libs/$APP_NAME-*.jar ../app-manifest-output/
 popd
 
-cat ./build-artifacts/manifests/manifest-bg.yml
+cat ./app-manifest-output/manifest-bg.yml
